8829b86fc5edd0f9859e5a83e57cf6df8f24a460
Esmaeill <liyanboy74@gmail.com>
2020-09-05 13:26:30 +0430

    add to git

---
+++ Core/Src/main.c
@@ -0,0 +1,405 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM2_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+typedef enum {
+	SoftUart_OK,
+	SoftUart_Error
+}SoftUartState_E;
+
+typedef struct {
+	uint8_t					Enable;
+	__IO uint8_t 		TxComplated;
+	uint8_t 				BitShift,BitConter;
+	uint8_t					Index,Size;
+	uint8_t* 				TxBuffer;
+	GPIO_TypeDef  	*TxPort;
+	uint16_t 				TxPin;
+} SoftUart_S;
+
+uint8_t SoftUart1TxBuffer[32];
+uint8_t SoftUart2TxBuffer[32];
+uint8_t SoftUart3TxBuffer[32];
+uint8_t SoftUart4TxBuffer[32];
+uint8_t SoftUart5TxBuffer[32];
+uint8_t SoftUart6TxBuffer[32];
+
+SoftUart_S SUart1={0,1,0,0,0,0,SoftUart1TxBuffer,GPIOB,GPIO_PIN_3};
+SoftUart_S SUart2={0,1,0,0,0,0,SoftUart2TxBuffer,GPIOB,GPIO_PIN_4};
+SoftUart_S SUart3={0,1,0,0,0,0,SoftUart3TxBuffer,GPIOB,GPIO_PIN_5};
+SoftUart_S SUart4={0,1,0,0,0,0,SoftUart4TxBuffer,GPIOB,GPIO_PIN_6};
+SoftUart_S SUart5={0,1,0,0,0,0,SoftUart5TxBuffer,GPIOB,GPIO_PIN_7};
+SoftUart_S SUart6={0,1,0,0,0,0,SoftUart6TxBuffer,GPIOB,GPIO_PIN_8};
+
+void SoftUartTransmitBit(SoftUart_S *SU,uint8_t Bit0_1)
+{
+	HAL_GPIO_WritePin(SU->TxPort,SU->TxPin,(GPIO_PinState)Bit0_1);
+}
+
+void SoftUartTxProcess(SoftUart_S *SU)
+{
+	if(SU->Enable)
+	{
+		if(SU->BitConter==0)
+		{
+			SU->TxComplated=0;
+			SU->BitShift=0;
+			SoftUartTransmitBit(SU,0);
+			SU->BitConter++;
+		}
+		else if(SU->BitConter<9)
+		{
+			SoftUartTransmitBit(SU,((SU->TxBuffer[SU->Index])>>(SU->BitShift))&0x01);
+			SU->BitConter++;
+			SU->BitShift++;
+		}
+		else if(SU->BitConter==9)
+		{
+			SoftUartTransmitBit(SU,1);
+			SU->BitConter++;
+		}
+		else if(SU->BitConter==10)
+		{
+			//Complate
+			SU->BitConter=0;;
+			
+			SU->Index++;
+			if(SU->Size > SU->Index)
+			{
+				SU->TxComplated=0;
+				SU->Enable=1;
+			}
+			else
+			{
+				SU->TxComplated=1;
+				SU->Enable=0;
+			}
+		}
+	}
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	if(htim->Instance==TIM2)
+	{
+		//HAL_GPIO_TogglePin(GPIOB,GPIO_PIN_5);
+		SoftUartTxProcess(&SUart1);
+		SoftUartTxProcess(&SUart2);
+		SoftUartTxProcess(&SUart3);
+		SoftUartTxProcess(&SUart4);
+		SoftUartTxProcess(&SUart5);
+		SoftUartTxProcess(&SUart6);
+	}
+}
+
+SoftUartState_E SoftUart_Putchar(SoftUart_S *SU,uint8_t Ch)
+{
+	if(!(SU->TxComplated)) return SoftUart_Error;
+	
+	SU->Index=0;
+	SU->Size=1;
+	SU->TxBuffer[0]= Ch;
+	
+	SU->BitConter=0;
+	
+	SU->TxComplated=0;
+	SU->Enable=1;
+	
+	//while(!(SU->TxComplated));
+	
+	return SoftUart_OK;
+}
+
+SoftUartState_E SoftUart_Puts(SoftUart_S *SU,uint8_t *Str,uint8_t Len)
+{
+	int i;
+	
+	if(!(SU->TxComplated)) return SoftUart_Error;
+	
+	SU->Index=0;
+	SU->Size=Len;
+	
+	for(i=0;i<Len;i++)
+	{
+		SU->TxBuffer[i]= Str[i];
+	}
+	
+	SU->BitConter=0;
+	
+	SU->TxComplated=0;
+	SU->Enable=1;
+	
+	//while(!(SU->TxComplated));
+	
+	return SoftUart_OK;
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM2_Init();
+  /* USER CODE BEGIN 2 */
+	HAL_TIM_Base_Start_IT(&htim2);
+	HAL_Delay(10);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+		//SoftUart_Putchar(&SUart1,0x41);
+		//SoftUart_Putchar(&SUart2,0x42);
+		
+		SoftUart_Puts(&SUart1,(uint8_t*)"Hello",5);
+		SoftUart_Puts(&SUart2,(uint8_t*)"My",2);
+		SoftUart_Puts(&SUart3,(uint8_t*)"Name",4);
+		SoftUart_Puts(&SUart4,(uint8_t*)"Is",2);
+		SoftUart_Puts(&SUart5,(uint8_t*)"Esmaeill",8);
+		SoftUart_Puts(&SUart6,(uint8_t*)"Maarfavi",8);
+		
+		//HAL_Delay(1);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 71;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 103;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6
+                          |GPIO_PIN_7|GPIO_PIN_8, GPIO_PIN_SET);
+
+  /*Configure GPIO pin : LED_Pin */
+  GPIO_InitStruct.Pin = LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB3 PB4 PB5 PB6
+                           PB7 PB8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6
+                          |GPIO_PIN_7|GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
